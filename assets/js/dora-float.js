9d8dd483d083824efb344a1bfb63133884c58e26
luizampv <luizampvicente@gmail.com>
2025-05-20 18:44:22 -0300

    Inclui o icone damascote Dora para permitir a navegação até sua página

---
+++ assets/js/dora-float.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dora = document.getElementById('dora-float');
+    if (dora) {
+        dora.addEventListener('click', function() {
+            window.location.href = './pages/Dora.html';
+        });
+    }
+});
